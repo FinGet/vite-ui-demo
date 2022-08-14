c94b9715b06509defc284a84228f1ddf8d1c4029
FinGet <zhangbiaoFT@163.com>
2022-08-14 14:12:17 +0800

    🎉 init: init demo

---
+++ src/button/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button>v 阿斯顿</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SFCButton"
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import SButton from "./button/button";
+import SFCButton from "./button/button.vue";
+import JSXButton from "./button/index";
+
+createApp(SFCButton).mount("#app");
